aa39fd0707f132d5740363d10bfac1220c25339c
Gina Giardinieri <ggiardinieri@gmail.com>
2018-05-17 17:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return 'Happy holidays, you!'
 }
 
-function happyHolidayTo(holiday) {
+function happyHolidayTo(holiday,name) {
   return 'Happy Independence Day, you!'
 }
 function holidayCountdown(holiday,days) {

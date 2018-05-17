8c892360e71ca6341b0ca6ab39c27b74d4236c37
Gina Giardinieri <ggiardinieri@gmail.com>
2018-05-17 16:41:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holiday) {
   return "Happy Independence Day, you!"
 }
 function holidayCountdown(holiday,days) {
-  return "It's ${days} days until ${holiday}!":
+  return `It's ${days} days until ${holiday}!`:
 }
